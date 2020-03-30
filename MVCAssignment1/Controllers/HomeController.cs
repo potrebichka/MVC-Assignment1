37b36f36d91057480b04df1bf160624dde5b1d75
Nina Potrebich <potrebich@gmail.com>
2020-03-30 12:39:51 -0700

    Create MVC project

---
+++ MVCAssignment1/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCAssignment1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index(string input)
+        {
+            if (input == "sample")
+                return File("Sample.pdf", "application/pdf");
+            else if (input == "gotoabout")
+                return RedirectToAction("About");
+            else if (input == "login")
+                return View("Login");
+            else
+                return Content($"You entered: " + input);
+        }
+
+        public ActionResult About()
+        {
+            return Content("About Content here", "text/plain");
+        }
+    }
+}
